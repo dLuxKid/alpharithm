2aa2f39cb2248bd6d80bff79445e10a90a5c2f29
dLuxKid <adetunjimarvellous09@gmail.com>
2025-03-22 21:17:17 +0100

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,12 +21,12 @@ export default function Home() {
             Leverage on automation
           </h4>
           <h1 className="font-semibold text-white text-center text-2xl sm:text-4xl md:text-[76px]/[82px] tracking-[-2%]">
-            AI Models for <br className="hidden sm:block" />
+            AI Models for <br className="hidden lg:block" />
             Business Solutions
           </h1>
           <p className="plus-jakarta font-medium text-white text-center text-lg md:text-[22px]/[38px] tracking-[-2%]">
             Leverage the power of AI to automate, analyze, and optimize your
-            workflows. Our <br className="hidden sm:block" /> specialized models
+            workflows. Our <br className="hidden lg:block" /> specialized models
             are designed are designed to fit different business needs.
           </p>
           <button className="bg-white rounded-xl text-black py-3.5 px-[26px] font-semibold text-lg transition-all cursor-pointer hover:bg-white/80">
@@ -36,14 +36,14 @@ export default function Home() {
       </div>
       <div className="min-h-screen w-full">
         <CompaniesCarousel />
-        <div className="h-full py-6 md:py-12 flex flex-col items-center justify-center gap-8 overflow-hidden">
+        <div className="h-full py-6 md:py-12 flex flex-col items-center justify-center gap-4 md:gap-8 overflow-hidden">
           <h2 className="mx-[2.5%] text-3xl sm:text-4xl md:text-[50px]/[62px] text-[#22263F] font-semibold tracking-[-2%] text-center">
             AI Models tailored for your <br className="hidden md:block" />{" "}
             business needs
           </h2>
           <p className="mx-[2.5%] font-medium text-[#828282] text-center text-lg md:text-[22px]/[38px] tracking-[-2%] plus-jakarta">
             Leverage the power of AI to automate, analyze, and optimize your
-            workflows. Our <br className="hidden sm:block" /> specialized models
+            workflows. Our <br className="hidden lg:block" /> specialized models
             are designed are designed to fit different business needs.
           </p>
           <TabsCarousel />
